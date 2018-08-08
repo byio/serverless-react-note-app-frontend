cab6537cb7289296a359bb2752a68bdf9ea344cb
byio <ben@benyang.io>
2018-08-09 05:11:48 +0800

    update state when texts in email and password fields are changed

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -17,6 +17,12 @@ export default class Login extends Component {
     return this.state.email.length > 0 && this.state.password.length > 0;
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="Login">
@@ -27,6 +33,7 @@ export default class Login extends Component {
               autoFocus
               type="email"
               value={this.state.email}
+              onChange={this.handleChange}
             />
           </FormGroup>
           <FormGroup controlId="password" bsSize="large">
@@ -34,6 +41,7 @@ export default class Login extends Component {
             <FormControl
               type="password"
               value={this.state.password}
+              onChange={this.handleChange}
             />
           </FormGroup>
           <Button
