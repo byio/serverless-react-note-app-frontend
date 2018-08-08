9caa6cc49555a3969bdd05c9b26aa145a0bb1ac5
byio <ben@benyang.io>
2018-08-08 21:52:22 +0800

    create Login container component with accompanying css file

---
+++ src/containers/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {  } from 'react-bootstrap';
+
+import './Login.css';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div className="Login">
+        Login component
+      </div>
+    );
+  }
+}
