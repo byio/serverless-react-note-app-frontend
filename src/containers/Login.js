5c3a2f7e2cfc2d86e9d1f0b6afe9d3055f684d14
byio <ben@benyang.io>
2018-08-08 22:05:06 +0800

    create Login form in Login container

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
-import {  } from 'react-bootstrap';
+import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 
 import './Login.css';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
   render() {
     return (
       <div className="Login">
-        Login component
+        <form>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              type="password"
+              value={this.state.password}
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
       </div>
     );
   }
