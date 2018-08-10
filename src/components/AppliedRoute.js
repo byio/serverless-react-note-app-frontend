da7c28878d2045c73bbcbbed1a1c943e68b7c418
byio <ben@benyang.io>
2018-08-10 10:15:20 +0800

    create AppliedRoutes component that essentially takes childProps, and component prop, and other props from Routes component and composes a Route component which renders the passed component with childProps and other props from the Routes component

---
+++ src/components/AppliedRoute.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+// export a function that accepts props from Routes component
+export default ({
+  component: C,
+  props: cProps,
+  ...rest
+}) => (
+  // function returns a Route component with props and a render function that returns the component passed through props from the Routes component
+  <Route
+    {...rest}
+    render={(props) => {
+      <C {...props} {...cProps} />
+    }}
+  />
+);
